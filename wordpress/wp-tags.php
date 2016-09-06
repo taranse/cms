fdee421de19fde17343d9d9c88dc55fcd289e551
taranse <bboy.stenli@yandex.ru>
2016-09-06 17:20:05 +0300

    Create wp-tags.php

---
+++ wordpress/wp-tags.php
@@ -0,0 +1,6 @@
+<!--Старт цикла-->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
+
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
