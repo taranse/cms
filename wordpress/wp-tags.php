8c4d4352801c38d84b9ac285c57c75066ab5cb28
taranse <bboy.stenli@yandex.ru>
2016-09-06 17:25:46 +0300

    Update wp-tags.php

--- wordpress/wp-tags.php
+++ wordpress/wp-tags.php
@@ -1,6 +1,16 @@
 <!--Старт цикла-->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
 
+<!--Конец Цикла-->
 <?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p> <!--Если нет постов-->
 <?php endif; ?>
+
+<!--Имя страницы-->
+<?php the_title(); ?>
+
+<!--Контент страницы-->
+<?php the_content(); ?>
+
+<!--Цитата страницы-->
+<?php the_excerpt(); ?>
